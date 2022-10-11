5404113ec5dd0159918ec8fb1f44d4633fdc5fcd
Alfonso Rebolleda Sánchez <47109499+arebolleda@users.noreply.github.com>
2022-10-11 10:33:24 +0200

    Add files via upload

---
+++ ut1-programacion multiproceso/Soluciones/prodletra.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+// Solucion Junior Reyes
+int  main()
+{
+   int  fp;
+   char  letra[]= "afasdfa\n";
+	
+    fp = open("FIFODAM",1);
+
+    printf("Mandando  informacion  al  FIFO...\n"); 
+    write(fp,letra,strlen(letra));
+   close(fp);
+   return 0;
+}
---
+++ ut1-programacion multiproceso/Soluciones/prodnumero.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+// Solucion Junior Reyes
+int  main()
+{
+   int  fp;
+   char  numero[]= "0123456789\n";
+	
+    fp = open("FIFODAM",1);
+
+    printf("Mandando  informacion  al  FIFO...\n"); 
+    write(fp,numero,strlen(numero));
+   close(fp);
+   return 0;
+}
